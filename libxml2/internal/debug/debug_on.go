8527cecae885b3a87e1d03bd93b17a34a10e2f02
leko <leko@atomite.io>
2021-12-06 11:55:39 +0100

    feat: add libxml2 dep

---
+++ libxml2/internal/debug/debug_on.go
@@ -0,0 +1,18 @@
+//go:build debug
+// +build debug
+
+package debug
+
+import (
+	"log"
+	"os"
+)
+
+const Enabled = true
+
+var logger = log.New(os.Stdout, "|DEBUG| ", 0)
+
+// Printf prints debug messages. Only available if compiled with "debug" tag
+func Printf(f string, args ...interface{}) {
+	logger.Printf(f, args...)
+}
---
+++ libxml2/libxml2.go
@@ -0,0 +1,2 @@
+//go:generate go run internal/cmd/genwrapnode/genwrapnode.go -- dom/node_wrap.go
+package libxml2
