ecb5d923a06408a11be347acc75757dbf1aed6dd
leko <leko@atomite.io>
2021-12-19 14:50:13 +0100

    chore: fix formatting

--- main.go
+++ main.go
@@ -1,56 +1,57 @@
 package greenlight
 
 import (
-  "path"
-  "strings"
+	"path"
+	"strings"
 
-  "github.com/spf13/viper"
+	"github.com/spf13/viper"
 )
 
 var (
-  configPaths = []string{
-    "$HOME/.greenlight",
-    "/etc/greenlight",
-    "/",
-    "/greenlight",
-    ".",
-  }
+	configPaths = []string{
+		"$HOME/.greenlight",
+		"/etc/greenlight",
+		"/",
+		"/greenlight",
+		".",
+	}
 )
 
-func stringsJoin(v string, o []string, joinHandler func (elem ...string) string) []string {
-  res := []string{}
+func stringsJoin(v string, o []string, joinHandler func(elem ...string) string) []string {
+	res := []string{}
 
-  for _, p := range o {
-    res = append(res, joinHandler(p, v))
-  }
+	for _, p := range o {
+		res = append(res, joinHandler(p, v))
+	}
 
-  return res
+	return res
 }
 
 func init() {
-  // default `logger`properties
-  viper.SetDefault("logger.level", "debug")
+	// default `logger`properties
+	viper.SetDefault("logger.level", "debug")
 
-  // default `schema` properties
-  viper.SetDefault("schema", "xsd/NeTEx_publication-NoConstraint.xsd")
+	// default `schema` properties
+	viper.SetDefault("schema", "xsd/NeTEx_publication-NoConstraint.xsd")
 
-  // default `scripts` properties
-  viper.SetDefault("scripts.enableBuiltIn", true)
-  viper.SetDefault("scripts.paths", stringsJoin("scripts", configPaths, path.Join))
+	// default `scripts` properties
+	viper.SetDefault("scripts.enableBuiltIn", true)
+	viper.SetDefault("scripts.builtInPath", "./builtin")
+	viper.SetDefault("scripts.paths", stringsJoin("scripts", configPaths, path.Join))
 
-  // set paths to look for configuration file (first come, first serve)
-  for _, p := range configPaths {
-    viper.AddConfigPath(p)
-  }
+	// set paths to look for configuration file (first come, first serve)
+	for _, p := range configPaths {
+		viper.AddConfigPath(p)
+	}
 
-  // name of configuration file, supported formats are JSON, TOML, YAML, HCL, INI, envfile or Java properties file
-  viper.SetConfigName("config")
+	// name of configuration file, supported formats are JSON, TOML, YAML, HCL, INI, envfile or Java properties file
+	viper.SetConfigName("config")
 
-  // if no configuration is found defaults will be used
-  viper.ReadInConfig()
+	// if no configuration is found defaults will be used
+	viper.ReadInConfig()
 
-  // read properties from environment
-  viper.SetEnvPrefix("GREENLIGHT")
-  viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-  viper.AutomaticEnv()
+	// read properties from environment
+	viper.SetEnvPrefix("GREENLIGHT")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 }
