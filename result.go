a4f8f1ba6ad1c5fe04ff6799a03e602f45724b1b
leko <leko@atomite.io>
2022-01-12 20:11:23 +0100

    refactor: move result to a new file

---
+++ result.go
@@ -0,0 +1,99 @@
+package greenlight
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type Measure struct {
+	start         time.Time
+	stop          time.Time
+	executionTime time.Duration
+}
+
+func (m *Measure) Start() { m.start = time.Now() }
+func (m *Measure) Stop() {
+	m.stop = time.Now()
+	m.executionTime = m.stop.Sub(m.start)
+}
+
+func (m *Measure) ExecutionTime() time.Duration { return m.executionTime }
+
+type ValidationResult struct {
+	*Measure
+
+	Name            string            `json:"file_name"`
+	Valid           bool              `json:"valid"`
+	GeneralError    string            `json:"general_error,omitempty"`
+	ValidationRules []*RuleValidation `json:"validation_rules,omitempty"`
+}
+
+func (r *ValidationResult) Markdown(extended bool) string {
+	valid := "✅"
+	if !r.Valid {
+		valid = "❌"
+	}
+
+	errorCount := 0
+	warningCount := 0
+
+	result := []string{}
+	for _, v := range r.ValidationRules {
+		if v.ErrorCount > 0 {
+			result = append(result, v.Markdown())
+			errorCount = errorCount + v.ErrorCount
+		}
+	}
+
+	rows := []string{
+		fmt.Sprintf("```\n%s\n```", r.Name),
+		fmt.Sprintf("- **valid**    -> %s", valid),
+		fmt.Sprintf("- **errors**   -> %d", errorCount),
+		fmt.Sprintf("- **warnings** -> %d\n", warningCount),
+	}
+
+	if extended && len(result) > 0 {
+		rows = append(rows, []string{
+			"\n*only rules w/ errors or warnings are shown below*\n",
+			"\n| Rule | Errors | Warnings |",
+			"| ---- | ------ | -------- |",
+		}...)
+		rows = append(rows, result...)
+	}
+
+	rows = append(rows, "*---*")
+
+	return strings.Join(rows, "\n")
+}
+
+type RuleValidation struct {
+	*Measure
+
+	Name        string   `json:"name"`
+	Description string   `json:"description,omitempty"`
+	Valid       bool     `json:"valid"`
+	ErrorCount  int      `json:"error_count,omitempty"`
+	Errors      []string `json:"errors,omitempty"`
+}
+
+func (r *RuleValidation) Markdown() string {
+	return fmt.Sprintf("| %s | %d | %d |", r.Name, r.ErrorCount, 0)
+}
+
+func (v *RuleValidation) AddError(err error) {
+	v.Valid = false
+	v.ErrorCount++
+
+	if v.ErrorCount <= 32 {
+		v.Errors = append(v.Errors, err.Error())
+	}
+}
+
+func generalValidationError(name string, err error) *ValidationResult {
+	return &ValidationResult{
+		Name:         name,
+		Valid:        false,
+		GeneralError: fmt.Sprintf("%s", err),
+	}
+}
--- worker.go
+++ worker.go
@@ -1,11 +1,11 @@
 package greenlight
 
 import (
-	"fmt"
 	"runtime"
-	"time"
 
+	"github.com/concreteit/greenlight/logger"
 	"github.com/lestrrat-go/libxml2/types"
+	"github.com/lestrrat-go/libxml2/xsd"
 )
 
 type task interface {
@@ -32,61 +32,23 @@ func startWorkers(tasks <-chan task, results chan<- interface{}) {
 
 type taskValidateDocument struct {
 	validator *Validator
+	ctx       *ValidationContext
 	name      string
+	document  types.Document
+}
+
+func (t taskValidateDocument) Execute(id int) interface{} {
+	return t.validator.ValidateDocument(t.name, t.document, t.ctx)
+}
+
+type taskScript struct {
+	script    *Script
+	schema    *xsd.Schema
+	logger    *logger.Logger
 	document  types.Document
 	documents map[string]types.Document
 }
 
-func (t taskValidateDocument) Execute(id int) interface{} {
-	return t.validator.Validate(t.document, t.name, t.documents)
-}
-
-type Measure struct {
-	start time.Time
-	stop  time.Time
-
-	ExecutionTimeMS int64 `json:"execution_time_ms"`
-}
-
-func (m *Measure) Start() { m.start = time.Now() }
-func (m *Measure) Stop() {
-	m.stop = time.Now()
-	m.ExecutionTimeMS = m.stop.Sub(m.start).Milliseconds()
-}
-
-type FileValidationResult struct {
-	*Measure
-
-	FileName        string              `json:"file_name"`
-	FileSize        int64               `json:"file_size"`
-	Valid           bool                `json:"valid"`
-	GeneralError    string              `json:"general_error,omitempty"`
-	ValidationRules []*ValidationResult `json:"validation_rules,omitempty"`
-}
-
-type ValidationResult struct {
-	*Measure
-
-	Name        string   `json:"name"`
-	Description string   `json:"description,omitempty"`
-	Valid       bool     `json:"valid"`
-	ErrorCount  int      `json:"error_count,omitempty"`
-	Errors      []string `json:"errors,omitempty"`
-}
-
-func (v *ValidationResult) AddError(err error) {
-	v.Valid = false
-	v.ErrorCount++
-
-	if v.ErrorCount <= 32 {
-		v.Errors = append(v.Errors, err.Error())
-	}
-}
-
-func generalValidationError(name string, err error) *FileValidationResult {
-	return &FileValidationResult{
-		FileName:     name,
-		Valid:        false,
-		GeneralError: fmt.Sprintf("%s", err),
-	}
+func (t taskScript) Execute(id int) interface{} {
+	return t.script.Execute(t.schema, t.logger, t.document, t.documents)
 }
