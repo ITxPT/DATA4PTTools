89df98bf1cfffc86cb49969a22deff0710454c0e
leko <leko@atomite.io>
2022-05-09 16:58:37 +0200

    add default local api url

--- app/hooks/useApiClient.ts
+++ app/hooks/useApiClient.ts
@@ -1,6 +1,6 @@
 import ApiClient from '../api/client';
 
-const client = new ApiClient(process.env.API_URL || '');
+const client = new ApiClient(process.env.API_URL || 'http://localhost:8080');
 
 const useApiClient = (): ApiClient => {
   return client;
