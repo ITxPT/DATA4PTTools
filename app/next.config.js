195d972a7e86ccc2a9781d9132d94007b9c47643
leko <leko@atomite.io>
2022-05-09 23:04:34 +0200

    increase build timeout

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 3600,
   env: {
     API_URL: process.env.API_URL,
     MQTT_URL: process.env.MQTT_URL,
