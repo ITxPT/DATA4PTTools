d612be60ba5723f0cce4699d653f85e0395a38dc
leko <leko@atomite.io>
2022-12-02 11:10:16 +0100

    refactor: fix config after dep upd

--- app/next.config.js
+++ app/next.config.js
@@ -4,12 +4,11 @@ const nextConfig = {
   staticPageGenerationTimeout: 3600,
   env: {
     API_URL: process.env.API_URL,
-    MQTT_URL: process.env.MQTT_URL,
+    MQTT_URL: process.env.MQTT_URL
   },
   images: {
-    loader: 'custom',
-    path: '',
-  },
+    loader: 'custom'
+  }
 }
 
 module.exports = nextConfig
