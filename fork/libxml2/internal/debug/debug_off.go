d7e0da83bb27f7260dc03030dcec16bbe43b5d46
leko <leko@13k0.com>
2023-02-20 08:49:33 +0100

    chore: remove replaced lib

--- fork/libxml2/internal/debug/debug_off.go
+++
@@ -1,9 +0,0 @@
-//go:build !debug
-// +build !debug
-
-package debug
-
-const Enabled = false
-
-// Printf is no op unless you compile with the `debug` tag
-func Printf(f string, args ...interface{}) {}
